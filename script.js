66ccea156533da3f56dd19ce7a84fe1125560868
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-27 16:46:19 -0700

    updated css, got add cities to work

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-$(document).ready(function() {
-
+$(document).ready(function(search) {
+citiesSearched = [];
      $("button").on("click", function() {
+    var search = $("input").val();
 
-    });
+    citiesSearched.push(search);
+    getWeather();
+      console.log(citiesSearched);
+    addCities(search);
+  });
+function addCities (search){
+var city = $("<div>").addClass("City-searched").text(search);
+$("aside").append(city);
+} 
+function getWeather (search){
+  // var weather =$(this).attr("data-name");
+  // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + search+ "&apikey=a93d83dd9db408417f860cd27c651c1b";
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // }).then(function(response) { });
 
 
-    // function displayMovieInfo() {
-
-    //     var movie = $(this).attr("data-name");
-    //     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-    //     // Creates AJAX call for the specific movie button being clicked
-    //     $.ajax({
-    //       url: queryURL,
-    //       method: "GET"
-    //     }).then(function(response) {
-
-
-
+}
+   
+getWeather();
 });
 
 
