840587f4a30bd68f1cf34483072e631b2f2c6a36
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-27 17:31:26 -0700

    created appends docs, made local storage

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ citiesSearched = [];
 function addCities (search){
 var city = $("<div>").addClass("City-searched").text(search);
 $("aside").append(city);
+localStorage.setItem("search",citiesSearched);
 } 
 function getWeather (search){
-  // var weather =$(this).attr("data-name");
-  // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + search+ "&apikey=a93d83dd9db408417f860cd27c651c1b";
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function(response) { });
-
-
+  
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + search+ "&apikey=a93d83dd9db408417f860cd27c651c1b";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) { });
+  $("#today").append();
+  $("#forcast").append();
 }
    
 getWeather();
