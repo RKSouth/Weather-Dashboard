0e7c7b2028efc08c760f168fbf9d4733dae151d1
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-27 20:20:02 -0700

    added comments and POA

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(search) {
 citiesSearched = [];
+
+
+//click the buttonn
      $("button").on("click", function() {
     var search = $("input").val();
 
@@ -8,22 +11,43 @@ citiesSearched = [];
       console.log(citiesSearched);
     addCities(search);
   });
-function addCities (search){
+
+
+//adds cities to the list below the search button
+  function addCities (search){
 var city = $("<div>").addClass("City-searched").text(search);
 $("aside").append(city);
 localStorage.setItem("search",citiesSearched);
 } 
+
+//gets the weather from openweathermap
 function getWeather (search){
   
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + search+ "&apikey=a93d83dd9db408417f860cd27c651c1b";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=48cb01e208735d9aa940904774b4bdabq="+ search;
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) { });
+  }).then(function(response) { 
+    
+    console.log(response);
+    console.log(queryURL);
+
+
+    var title =$("<h1>").text(response.name);
+    $("#city").append(title);
+  });
   $("#today").append();
   $("#forcast").append();
 }
-   
+
+//"main.dt_txt" is date
+//need temp
+//need humidty
+//need uv index
+//need wind speed
+//need weather: main attached to image if statements!
+//Loop through the forecast list array and display a single forecast entry/time (5th entry of each day which is close to the highest temp/time of the day) from each of the 5 days
+
 getWeather();
 });
 
